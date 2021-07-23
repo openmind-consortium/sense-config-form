82678deb028f51ae3cff9639f34de095a357502d
Ivo Su <ivosu@Ivos-MBP.attlocal.net>
2021-07-23 12:07:26 -0400

    feat: initial schema and form

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ConfigForm from './components/ConfigForm'
+
+function App() {
+  return (
+    <div className='main-window'>
+      <ConfigForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/components.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
